ec4c773fdcb7a44dec108cd5766a62907c0b463b
Peti <Vpeter993@gmail.com>
2025-09-12 22:23:52 +0200

    refactor: Replace model_viewer with flutter_cube

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,55 @@
-// Import the Flutter Material library, which contains all the standard widgets.
 import 'package:flutter/material.dart';
+import 'package:flutter_cube/flutter_cube.dart';
 
-// The main function is the starting point for all Flutter apps.
-void main(){
+void main() {
   runApp(const SynapseApp());
 }
 
-// This is the root widget of the application.
 class SynapseApp extends StatelessWidget {
   const SynapseApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // We remove the debug banner from the top right corner.
       debugShowCheckedModeBanner: false,
-      // We set the entire app's theme to dark to start our space theme.
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF0A0A14), // A deep navy blue for our space
+        scaffoldBackgroundColor: const Color(0xFF0A0A14), // A deep navy blue
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent, // Make app bar transparent
-          elevation: 0, // Remove shadow
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
       ),
-      // The home property defines what the user sees on the main screen.
       home: const HomeScreen(),
     );
   }
 }
 
-// This widget represents the main screen of our app.
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Scene _scene;
+
+  @override
   Widget build(BuildContext context) {
-    // Scaffold is a basic page layout widget.
     return Scaffold(
-      // AppBar is the bar at the top of the screen.
       appBar: AppBar(
         title: const Text('Synapse'),
       ),
-      // Center widget aligns its child (the Text) to the middle of the screen.
-      body: const Center(
-        child: Text(
-          'Our Universe Awaits...',
-          style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
-        ),
+      // CORRECTED: Use the 'Cube' widget from the library, not 'Scene'.
+      body: Cube(
+        // CORRECTED: The parameter is 'onSceneCreated'.
+        onSceneCreated: (Scene scene) {
+          _scene = scene;
+          // We can move the camera back a bit to see more of the space.
+          scene.camera.position.z = 10;
+        },
       ),
     );
   }
 }
+
